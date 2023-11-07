b0446cc39531a5c1e08c71c7ba969aaed6e4cb1a
Andrew Magid <magid.andrew@gmail.com>
2023-11-07 10:26:26 -0500

    fixed function and chain switching

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -26,7 +26,7 @@ export const ZEEKAPTCHA_CHAIN_CONSTANTS = {
   sepolia: {
     address: "0x49527e13101DA9973fEd8170b719584b7429D191",
     abi: [{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"EOA","type":"address"}],"name":"CaptchaCompleted","type":"event"},{"inputs":[{"internalType":"uint256[24]","name":"_proof","type":"uint256[24]"},{"internalType":"uint256[2]","name":"_pubSignals","type":"uint256[2]"}],"name":"verifyProof","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}],
-    chainId: 11155111
+    chainId: "0xaa36a7"
   }
 }
 
--- src/utils/transaction.tsx
+++ src/utils/transaction.tsx
@@ -5,12 +5,16 @@ export const submitTransaction = async (_proof: string[], _pubSignals: string[])
     const contractAddress = ZEEKAPTCHA_CHAIN_CONSTANTS.sepolia.address
     const abi = ZEEKAPTCHA_CHAIN_CONSTANTS.sepolia.abi
     if ((window as any).ethereum) {
+        await (window as any).ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: ZEEKAPTCHA_CHAIN_CONSTANTS.sepolia.chainId }],
+        });
         (window as any).ethereum.request({ method: 'eth_requestAccounts' })
             .then(async () => {
                 const provider = new ethers.BrowserProvider((window as any).ethereum)
                 const signer = await provider.getSigner();
                 const contract = new ethers.Contract(contractAddress, abi, signer)
-                const tx = await contract.verifyInstant(_proof, _pubSignals, {gasLimit: 350000})
+                const tx = await contract.verifyProof(_proof, _pubSignals, {gasLimit: 350000})
                 await tx.wait()
                 console.log(tx.hash)
             })
