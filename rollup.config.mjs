b32031e466504c03a7632d9a693403bce1e71188
Andrew Magid <magid.andrew@gmail.com>
2023-11-06 23:30:33 -0500

    uninstalled rollup deps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,9 @@
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';
 import image from "@rollup/plugin-image";
-import url from '@rollup/plugin-url';
 import { base64 } from 'rollup-plugin-base64';
 
 
